65b1f6a3e91f044b78c64450b62c64a8a397b85c
Ken <kel89@cornell.edu>
2024-10-17 10:49:17 -0600

    query added for contact ingo

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,10 @@
-export default function Contact() {
+import { getContactData } from "../utils/sanityClient";
+
+export default async function Contact() {
+    const { email, phone, address, imageUrl } = await getContactData();
     return (
         <>
             <h1>Contact</h1>
         </>
-    )
+    );
 }
--- app/utils/sanityClient.js
+++ app/utils/sanityClient.js
@@ -1,40 +1,51 @@
 // sanity.js
-import { createClient } from '@sanity/client'
-import imageUrlBuilder from '@sanity/image-url'
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 // Import using ESM URL imports in environments that supports it:
 // import {createClient} from 'https://esm.sh/@sanity/client'
 
 export const client = createClient({
-  projectId: 'geus6dp5',
-  dataset: 'production',
-  useCdn: true, // set to `false` to bypass the edge cache
-  apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
-  // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
-})
+    projectId: "geus6dp5",
+    dataset: "production",
+    useCdn: true, // set to `false` to bypass the edge cache
+    apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
+    // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+});
 
 export function urlFor(source) {
-  const builder = imageUrlBuilder(client)
-  return builder.image(source)
+    const builder = imageUrlBuilder(client);
+    return builder.image(source);
 }
 
 export async function getPortfolioImages() {
-  const data = await client.fetch('*[_type == "portfolioPage"]{images}')
-  const images = data[0]?.images.map(image => urlFor(image).url())
-  return images
+    const data = await client.fetch('*[_type == "portfolioPage"]{images}');
+    const images = data[0]?.images.map((image) => urlFor(image).url());
+    return images;
+}
+
+export async function getContactData() {
+    let data = await client.fetch('*[_type == "contact"]');
+    data = data[0];
+    return {
+        email: data.email,
+        phone: data.phone,
+        address: data.address,
+        imageUrl: urlFor(data.image).url(),
+    };
 }
 
 // uses GROQ to query content: https://www.sanity.io/docs/groq
 export async function getPosts() {
-  const posts = await client.fetch('*[_type == "post"]')
-  return posts
+    const posts = await client.fetch('*[_type == "post"]');
+    return posts;
 }
 
 export async function createPost(post) {
-  const result = client.create(post)
-  return result
+    const result = client.create(post);
+    return result;
 }
 
 export async function updateDocumentTitle(_id, title) {
-  const result = client.patch(_id).set({title})
-  return result
+    const result = client.patch(_id).set({ title });
+    return result;
 }

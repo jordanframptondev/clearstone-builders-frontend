c38f6b7c176994afa2662129a9026566030690a5
jordan-frampton <jordan.frampton@flybreeze.com>
2024-10-18 01:07:29 -0600

    added home screen image transition

--- app/page.js
+++ app/page.js
@@ -21,11 +21,11 @@ export default function Home() {
   useEffect(() => {
     // List of image file names in the public/static folder
     const imageFiles = [
-      "/static/home-1.jpg",
-      "/static/home-2.jpg",
-      "/static/home-3.jpg",
-      "/static/home-4.jpg",
-      "/static/home-5.jpg",
+      "https://images.squarespace-cdn.com/content/v1/55f739f4e4b0550fcc13832a/1555510104579-3OHHEQS6TW8VGNBDVWIN/remodel-building-luxury-living-shenandoah-valley-virginia",
+      "https://www.redfin.com/blog/wp-content/uploads/2023/01/new_hope_home_1-1024x683.jpg",
+      "https://homesbymidtown.com/wp-content/uploads/2021/05/Exterior-Front.jpg",
+      "https://utahvalleyrealestateforsale.com/wp-content/uploads/2023/11/Orem-Utah-Homes-for-Sale-scaled.jpeg",
+      "https://ssl.cdn-redfin.com/photo/269/islphoto/870/genIslnoResize.3581870_0.jpg",
     ];
     console.log("images", imageFiles);
     setImages(imageFiles);
@@ -35,36 +35,43 @@ export default function Home() {
     if (images?.length > 0) {
       const interval = setInterval(() => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 3000);
+      }, 4000);
 
       return () => clearInterval(interval);
     }
   }, [images?.length]);
 
   return (
-    // <div className="image-container">
-    //   {static?.map((image, index) => (
-    //     <div
-    //       key={index}
-    //       className={`image-slide ${index === currentIndex ? "active" : ""}`}
-    //     >
-    //       <Image
-    //         src={image}
-    //         alt={`Image ${index}`}
-    //         layout="fill"
-    //         objectFit="cover"
-    //       />
-    //     </div>
-    //   ))}
-    // </div>
 
-    <div className="image-container">
-          <Image
-            src="/static/home-1.jpg"
-            alt="Image 1"
-            layout="fill"
-            objectFit="cover"
-          />
-    </div>
+      // <div className="image-container">
+      //   {static?.map((image, index) => (
+      //     <div
+      //       key={index}
+      //       className={`image-slide ${index === currentIndex ? "active" : ""}`}
+      //     >
+      //       <Image
+      //         src={image}
+      //         alt={`Image ${index}`}
+      //         layout="fill"
+      //         objectFit="cover"
+      //       />
+      //     </div>
+      //   ))}
+      // </div>
+
+      <div className="image-container">
+        {images.map((image, index) => (
+            <div
+                key={index}
+                className={`image-slide ${index === currentIndex ? "active" : ""}`}
+            >
+              <img
+                  src={image}
+                  alt={`Image ${index}`}
+                  className="full-screen-image"
+              />
+            </div>
+        ))}
+      </div>
   );
 }

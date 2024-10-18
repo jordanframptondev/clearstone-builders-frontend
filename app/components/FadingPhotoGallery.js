0490e7bb86915e783740d4879b6dd16711fe0cbf
jordan-frampton <jordan.frampton@flybreeze.com>
2024-10-18 01:51:14 -0600

    split fading photos into its own component

---
+++ app/components/FadingPhotoGallery.js
@@ -0,0 +1,39 @@
+"use client";
+
+import "@/app/styles/fading-photo-gallery.css";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export function FadingPhotoGallery({ imageURLs }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (imageURLs?.length > 0) {
+      const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % imageURLs.length);
+      }, 3500);
+
+      return () => clearInterval(interval);
+    }
+  }, [imageURLs?.length]);
+
+  return (
+    <div className="image-container">
+      {imageURLs.map((image, index) => (
+        <div
+          key={index}
+          className={`image-slide ${index === currentIndex ? "active" : ""}`}
+        >
+          <Image
+            src={image}
+            width={1600}
+            height={900}
+            alt={`Image ${index}`}
+            className="full-screen-image"
+            priority={true}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,80 +1,17 @@
-"use client";
-
-import "./style.css";
-import Image from "next/image";
 import { getHomeImages } from "@/app/utils/cms-service";
-import { useEffect, useState } from "react";
+import {FadingPhotoGallery} from "@/app/components/FadingPhotoGallery";
 
 export default function Home() {
-  const [images, setImages] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
 
-  // useEffect(() => {
-  //   async function fetchImages() {
-  //     const fetchedImages = await getHomeImages();
-  //     setImages(fetchedImages);
-  //     console.log("images", imageFiles);
-  //   }
-  //
-  //   fetchImages();
-  // }, []);
+  const images = [
+    "https://cdn.sanity.io/images/geus6dp5/production/334d7caf40d73db63a813fcac9ed753980cd8a6c-8000x4501.png",
+    "https://cdn.sanity.io/images/geus6dp5/production/1f633622353a3779ce9419461cb7eec6ac6c096d-8001x4501.png",
+    "https://cdn.sanity.io/images/geus6dp5/production/5f2dcb6253ab41d158d0011cbd65edf134b6a153-8000x4501.png",
+    "https://cdn.sanity.io/images/geus6dp5/production/cbb9c4376c3f1fd6393ac85ab0c23ae1a9e87c7c-8001x4501.png",
+    "https://cdn.sanity.io/images/geus6dp5/production/5534a394a1171e410ff1372fea8541f8e0a05c01-8000x4501.png",
+  ];
 
-  useEffect(() => {
-    // List of image file names in the public/static folder
-    const imageFiles = [
-      "https://cdn.sanity.io/images/geus6dp5/production/334d7caf40d73db63a813fcac9ed753980cd8a6c-8000x4501.png",
-      "https://cdn.sanity.io/images/geus6dp5/production/1f633622353a3779ce9419461cb7eec6ac6c096d-8001x4501.png",
-      "https://cdn.sanity.io/images/geus6dp5/production/5f2dcb6253ab41d158d0011cbd65edf134b6a153-8000x4501.png",
-      "https://cdn.sanity.io/images/geus6dp5/production/cbb9c4376c3f1fd6393ac85ab0c23ae1a9e87c7c-8001x4501.png",
-      "https://cdn.sanity.io/images/geus6dp5/production/5534a394a1171e410ff1372fea8541f8e0a05c01-8000x4501.png",
-    ];
-    console.log("images", imageFiles);
-    setImages(imageFiles);
-  }, []);
-
-  useEffect(() => {
-    if (images?.length > 0) {
-      const interval = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 3500);
-
-      return () => clearInterval(interval);
-    }
-  }, [images?.length]);
-
-  return (
-    // <div className="image-container">
-    //   {static?.map((image, index) => (
-    //     <div
-    //       key={index}
-    //       className={`image-slide ${index === currentIndex ? "active" : ""}`}
-    //     >
-    //       <Image
-    //         src={image}
-    //         alt={`Image ${index}`}
-    //         layout="fill"
-    //         objectFit="cover"
-    //       />
-    //     </div>
-    //   ))}
-    // </div>
-
-    <div className="image-container">
-      {images.map((image, index) => (
-        <div
-          key={index}
-          className={`image-slide ${index === currentIndex ? "active" : ""}`}
-        >
-          <Image
-            src={image}
-            width={1600}
-            height={900}
-            alt={`Image ${index}`}
-            className="full-screen-image"
-            priority={true}
-          />
-        </div>
-      ))}
-    </div>
-  );
+  // TODO - get fetch images from CMS working
+  // const cmsImages = getHomeImages();
+  return <FadingPhotoGallery imageURLs={images} />;
 }

9e5c6548ffe8e9d7fd5295661f5927334289cff1
Tom Hall <thall1961@gmail.com>
2024-11-07 09:46:28 -0600

    npm prep

--- components/PhotoGallery.js
+++ components/PhotoGallery.js
@@ -4,16 +4,28 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useWindowSize } from '../app/hooks/useWindowSize';
 
-export function PhotoGallery({ photos }) {
+export function PhotoGallery({ photos, xlColumns, lgColumns, mdColumns, smColumns }) {
   const [selectedImage, setSelectedImage] = useState(null);
   const { width } = useWindowSize();
-  const defaultWidth = width ? (width > 600 ? 3 : 1) : 3
-  const [columns, setColumns] = useState(defaultWidth);
+  const [columns, setColumns] = useState(1);
   console.log("WIDTH", width, "COLUMNS", columns);
 
   useEffect(() => {
-    setColumns(width > 600 ? 3 : 1);
-  }, [width])
+    const columnWidth = getColumnsWidth()
+    setColumns(columnWidth);
+  }, [getColumnsWidth, width])
+
+  const getColumnsWidth = useCallback(() => {
+    if (width > 1200 && xlColumns) {
+      return xlColumns;
+    } else if (width > 992 && lgColumns) {
+      return lgColumns;
+    } else if (width > 768 && mdColumns) {
+      return mdColumns;
+    } else {
+      return smColumns || 1;
+    }
+  }, [lgColumns, mdColumns, smColumns, width, xlColumns]);
 
   const openLightbox = (image) => setSelectedImage(image);
   const closeLightbox = () => setSelectedImage(null);
@@ -32,7 +44,7 @@ export function PhotoGallery({ photos }) {
 
   return (
     <>
-      { columns ? <div className={`${columns ? 'columns-' + columns : 'columns-3'} gap-4`}>
+      { columns ? <div className={`${columns ? 'columns-' + columns : 'columns-1'} gap-4`}>
         {photosInOrder.map((photo, index) => (
           <div key={index} className="relative cursor-pointer overflow-hidden mb-4">
             <Image

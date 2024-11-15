2955f9acdbd7deb1b267a3a8d2a8568580ea86a9
jordan-frampton <jordan.frampton@flybreeze.com>
2024-11-15 11:59:25 -0700

    new focus page overflow scroll

--- app/focus/page.js
+++ app/focus/page.js
@@ -15,24 +15,24 @@ export default async function Contact() {
     const textHtml = toHTML(text);
     return (
         <>
-            <div className="image-container">
+            <div className="fixed top-0 left-0 w-[100dvw] h-[100dvh]">
                 <Image
                     src={imageUrl}
                     alt="Background Image"
                     width={1920}
                     height={1080}
                     priority={true}
-                    className={"fade-in-background focus-page-full-screen-image"}
+                    className="fade-in-background absolute inset-0 h-full w-full object-cover"
                 />
             </div>
 
             <div
-                className="flex items-center absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 transition-all duration-500 ease-out
+                className="textbox transition-all duration-500 ease-out
                 max-w-[1260px] w-full lg:min-w-[1000px]">
-                <div className="bg-[#262a1cbd] transition-all duration-500 ease-out
-                fade-in-div bg-opacity-80 px-9 py-10 rounded-sm text-left text-white font-light overflow-auto">
+                <div className="mb-[150px] z-10 bg-[#262a1cbd] transition-all duration-500 ease-out
+                fade-in-div bg-opacity-80 px-9 py-10 rounded-sm text-left text-white font-light">
                     <div
-                        className="text-[18px] lg:text-[22px] custom-paragraph-spacing opacity-70 leading-[28px] max-height-constraint"
+                        className="text-[18px] lg:text-[22px] custom-paragraph-spacing opacity-70 leading-[28px]"
                         dangerouslySetInnerHTML={{__html: textHtml}}
                     />
                 </div>

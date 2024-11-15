dd98d3700b666007eb220886790ca61069fb4a24
jordan-frampton <jordan.frampton@flybreeze.com>
2024-11-15 12:17:33 -0700

    keep header fixed

--- components/Header.js
+++ components/Header.js
@@ -50,7 +50,7 @@ export function Header() {
 
     return (
         <>
-            <div className={`sticky top-0 z-50 w-full`}>
+            <div className={`fixed top-0 z-50 w-full`}>
                 <div
                     id="icon"
                     ref={iconRef}
